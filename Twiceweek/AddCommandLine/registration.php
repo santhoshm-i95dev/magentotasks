ef16fc84c0924d6d13732cd5c48379d60c214e49
santhoshm-i95dev <98160113+santhoshm-i95dev@users.noreply.github.com>
2022-08-16 19:24:20 +0530

    Add files via upload

---
+++ Twiceweek/AddCommandLine/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Twiceweek_AddCommandLine',
+    __DIR__
+);
+?>
---
+++ Twiceweek/CustomIndexer/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Twiceweek_CustomIndexer',
+    __DIR__
+);
+?>
